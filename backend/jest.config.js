2dea28b210a8eea6b0dd16b6328d0af1dd82b9b0
iso123-sprik <bikandacharlyisaac@gmail.com>
2026-07-06 05:06:46 +0200

    Add ESLint, .gitignore, and additional configuration files

---
+++ backend/jest.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/**/*.interface.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@middleware/(.*)$': '<rootDir>/src/middleware/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@database/(.*)$': '<rootDir>/src/database/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        esModuleInterop: true,
+      },
+    },
+  },
+};
